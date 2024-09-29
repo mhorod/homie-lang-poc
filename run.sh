9ec02b0844bae495e461b286a3e89bfc58d73480
Kacper Topolski <kacpertopolski@op.pl>
2024-09-29 02:54:46 +0200

    Fix nonzero return code

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ gcc -o build/libhomie.o -c -nostdlib -fno-stack-protector libhomie.c &&
 python3 src/main.py $1 > build/main.asm &&
 nasm -f elf64 -o build/main.o build/main.asm &&
 ld build/main.o build/libhomie.o -o build/program.out) || exit 1
-./build/program.out
+./build/program.out || true
